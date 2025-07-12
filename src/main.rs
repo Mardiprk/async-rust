6debdc4c3cfeb3dab2ff821cb5bd5abe5f2f5b8e
Mardiprk <mk1659770@gmail.com>
2025-07-12 22:49:50 +0530

    Tokioooooo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::time::{sleep, Duration};
+
+#[tokio::main]
+async fn main(){
+    println!("START!");
+    say_hello().await;
+    println!("END!");
+}
+
+async fn say_hello(){
+    println!("Hello from Async Function!");
+    sleep(Duration::from_secs(5)).await;
+    println!("Finished after 5 sec wait!");
 }
