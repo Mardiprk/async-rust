c54d86c7679af1c29fdf94e2283a25c3d231d0e2
Mardiprk <mk1659770@gmail.com>
2025-07-13 00:24:04 +0530

    tokiooo

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,25 @@ use tokio::time::{sleep, Duration};
 
 #[tokio::main]
 async fn main(){
-    println!("START!");
-    say_hello().await;
-    println!("END!");
-}
+    println!("Starting tasks...");
 
-async fn say_hello(){
-    println!("Hello from Async Function!");
-    sleep(Duration::from_secs(5)).await;
-    println!("Finished after 5 sec wait!");
+    let task1 = tokio::spawn(async{
+        println!("Task 1 Making Noodles..");
+        sleep(Duration::from_secs(2)).await;
+        println!("Task 1 Done!");
+        42
+    });
+
+    let task2 = tokio::spawn(async{
+        println!("Task 2 Making Sushi...");
+        sleep(Duration::from_secs(10)).await;
+        println!("Task 2 Done!");
+        100
+    });
+
+    let res1 = task1.await.unwrap();
+    let res2 = task2.await.unwrap();
+
+    println!("📦 Results: Task 1 = {:?}, Task 2 = {:?}", res1, res2);
+    println!("🎉 All tasks done!");
 }
